bc5c9a6a9226170469b2fe345a675af4cefec7b6
Flynn <flynn@Flynns-MacBook-Pro.local>
2018-06-28 10:26:25 +1000

    Latest

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // dependencies
 const express = require('express');
-const routes = require('./routes/index');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors')
 
 const { initializePassport } = require('./middleware/auth')
 
@@ -12,6 +12,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use(initializePassport)
+app.use(cors())
 
 // mongoose
 mongoose.connect('mongodb://localhost/express-mongo-passport', (err) => {
@@ -22,6 +23,7 @@ mongoose.connect('mongodb://localhost/express-mongo-passport', (err) => {
   }
 });
 
-app.use('/', routes);
+app.use('/auth', require('./routes/auth'))
+app.use('/bookmarks', require('./routes/bookmarks'))
 
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
